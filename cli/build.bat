38b9fc22a6e1f10c101ae7d78f8c1c9218a6ccf5
Stewan Pacheco <talk@stpa.co>
2015-05-28 13:12:13 -0300

    cli

--- cli/build.bat
+++ cli/build.bat
@@ -1,9 +1,9 @@
 cd ../
 gulp build
-rm -rfv ../.build/*
-cp -rfv dist/doc/* ../.build
-cp dist/doc/index.html ../.build/404.html
-cd ../.build
+rm -rfv .build/*
+cp -rfv dist/doc/* .build
+cp dist/doc/index.html .build/404.html
+cd .build
 git add -A
 git commit -am "new doc build"
 git push --force origin gh-pages
