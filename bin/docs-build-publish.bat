fbc2e243c8bd5c0e00846abc6d2c48c16c61dea4
stewones <talk@stpa.co>
2016-01-25 18:30:28 -0200

    fix(doc/build)

--- bin/docs-build-publish.bat
+++ bin/docs-build-publish.bat
@@ -1,6 +1,7 @@
-rm -rfv ../.build
-mkdir ../.build
-cd ../.build
+cd ..
+rm -rfv .build
+mkdir .build
+cd .build
 git init
 git remote add origin https://github.com/stewones/angular-morris-chart
 git checkout -b gh-pages
